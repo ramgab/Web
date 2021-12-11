95df29e6c4ead0f450f813470f08f51b268959a1
ramgab <90207508+ramgab@users.noreply.github.com>
2021-12-11 14:43:35 +0300

    Update tic tac toe.js
    Сначала вводятся координаты, после чего на странице появляется игровое поле

--- Семинар 13-14 (Крестики-нолики)/tic tac toe.js
+++ Семинар 13-14 (Крестики-нолики)/tic tac toe.js
@@ -3,84 +3,101 @@ let board = [[".", ".", "."],
          [".", ".", "."]];
 let stop = 0; //Если стоп будет равна 1, то кто-то выиграл, в противном случае объявится ничья 
 for (let y = 0; y < 3; y++){ //Вывод поля на экран
-    console.log(board[y][0] + ' ' + board[y][1] + ' ' + board[y][0]);  
+    document.write(board[y][0] + ' ' + board[y][1] + ' ' + board[y][0]);
+    document.write('<br/>')  
 }
 //Приступаем к ходу и проверяем, выиграл ли кто-то или заполнено ли поле
 while (stop == 0 || (board[0][0] == '.' && board[0][1] == '.' && board[0][2] == '.' && board[1][0] == '.' && board[1][1] == '.' && board[1][2] == '.' && board[2][0] == '.' && board[2][1] == '.' && board[2][2] == '.')){
         if (stop != 1 && (board[0][0] == '.' || board[0][1] == '.' || board[0][2] == '.' || board[1][0] == '.' || board[1][1] == '.' 
                           || board[1][2] == '.' || board[2][0] == '.' || board[2][1] == '.' || board[2][2] == '.')){
-            console.log("ход крестиков ");
+            document.write("ход крестиков "); 
+            document.write('<br/>')
             let line = Number(prompt("Введите номер линии: "));
             let column = Number(prompt("Введите номер столбца: "));
             while (line > 3 || line < 1 || column > 3 || column < 1){ //Если выбранная позиция не существует
-                console.log("Введите координаты заново, выбранная позиция не существует");
+                document.write("Введите координаты заново, выбранная позиция не существует");
+                document.write('<br/>')
                 let line = Number(prompt("Введите номер линии: "));
                 let column = Number(prompt("Введите номер столбца: "));
             }    
             while (board[line-1][column-1] != '.'){ //Если выбранная позиция уже занята, то просим игрока заново ввести координаты
-                console.log("Введите координаты заново, выбранная позиция занята");
+                document.write("Введите координаты заново, выбранная позиция занята");
+                document.write('<br/>')
                 let line = Number(prompt("Введите номер линии: "));
                 let column = Number(prompt("Введите номер столбца: "));
             }    
             board[line-1][column-1] = 'x';
             for (let y = 0; y < 3; y++){ //Вывод обновленного поля на экран
-                console.log(board[y][0] + ' ' + board[y][1] + ' ' + board[y][0]);  
+                document.write(board[y][0] + ' ' + board[y][1] + ' ' + board[y][0]); 
+                document.write('<br/>') 
             }
 
         }else{
             stop = 2; //Чтобы остановить цикл при ничье
         }    
         if (board[0][0] == 'x' && board[0][1] == 'x' && board[0][2] == 'x'){ //Проверка строк
-            console.log("победили крестики!");
+            document.write("победили крестики!");
+            document.write('<br/>')
             stop = 1;
         }    
         else if (board[1][0] == 'x' && board[1][1] == 'x' && board[1][2] == 'x'){
-            console.log("победили крестики!");
+            document.write("победили крестики!");
+            document.write('<br/>')
             stop = 1;
         }    
         else if (board[2][0] == 'x' && board[2][1] == 'x' && board[2][2] == 'x'){
-            console.log("победили крестики!"); 
+            document.write("победили крестики!"); 
+            document.write('<br/>')
             stop = 1;
         }    
         else if (board[0][0] == 'x' && board[1][0] == 'x' && board[2][0] == 'x'){ //Проверка столбцов
-            console.log("победили крестики!");
+            document.write("победили крестики!");
+            document.write('<br/>')
             stop = 1;
         }    
         else if (board[0][1] == 'x' && board[1][1] == 'x' && board[2][1] == 'x'){
-            console.log("победили крестики!");
+            document.write("победили крестики!");
+            document.write('<br/>')
             stop = 1;
         }    
         else if (board[0][2] == 'x' && board[1][2] == 'x' && board[2][2] == 'x'){
-            console.log("победили крестики!");
+            document.write("победили крестики!");
+            document.write('<br/>')
             stop = 1;
         }    
         else if (board[0][0] == 'x' && board[1][1] == 'x' && board[2][2] == 'x'){ //Проверка диагоналей
-            console.log("победили крестики!");
+            document.write("победили крестики!");
+            document.write('<br/>')
             stop = 1;
         }    
         else if (board[0][2] == 'x' && board[1][1] == 'x' && board[2][0] == 'x'){
-            console.log("победили крестики!");
+            document.write("победили крестики!");
+            document.write('<br/>')
             stop = 1;
         }
         
         if (stop != 1 && (board[0][0] == '.' || board[0][1] == '.' || board[0][2] == '.' || board[1][0] == '.' || board[1][1] == '.' 
                           || board[1][2] == '.' || board[2][0] == '.' || board[2][1] == '.' || board[2][2] == '.')){ //Для того, чтобы после победы крестиков не просили ходить нолики
-            console.log("ход ноликов ");
+            document.write("ход ноликов ");
+            document.write('<br/>')
             line = Number(prompt("Введите номер линии: "));
             column = Number(prompt("Введите номер столбца: "));
             while (line > 3 || line < 1 || column > 3 || column < 1){ //Если выбранная позиция не существует
-                console.log("Введите координаты заново, выбранная позиция не существует");
+                document.write("Введите координаты заново, выбранная позиция не существует");
+                document.write('<br/>')
                 line = Number(prompt("Введите номер линии: "));
                 column = Number(prompt("Введите номер столбца: "));
             }    
             while (board[line-1][column-1] != '.'){ //Если выбранная позиция уже занята, то просим игрока заново ввести координаты; либо если на поле нет больше мест
-                console.log("Введите координаты заново, выбранная позиция занята");
+                document.write("Введите координаты заново, выбранная позиция занята");
+                document.write('<br/>')
                 line = Number(prompt("Введите номер линии: "));
                 column = Number(prompt("Введите номер столбца: "));
             }    
             board[line-1][column-1] = 'o';
             for (let y = 0; y < 3; y++){ //Вывод обновленного поля на экран
-                console.log(board[y][0] + ' ' + board[y][1] + ' ' + board[y][0]);  
+                document.write(board[y][0] + ' ' + board[y][1] + ' ' + board[y][0]);  
+                document.write('<br/>')
             }
                 
         }else{
@@ -89,41 +106,49 @@ while (stop == 0 || (board[0][0] == '.' && board[0][1] == '.' && board[0][2] == 
             }    
         }        
         if (board[0][0] == 'o' && board[0][1] == 'o' && board[0][2] == 'o'){ //Проверка строк
-            console.log("победили нолики!");
+            document.write("победили нолики!");
+            document.write('<br/>')
             stop = 1;
         }    
         else if (board[1][0] == 'o' && board[1][1] == 'o' && board[1][2] == 'o'){
-            console.log("победили нолики!");
+            document.write("победили нолики!");
+            document.write('<br/>')
             stop = 1;
         }    
         else if (board[2][0] == 'o' && board[2][1] == 'o' && board[2][2] == 'o'){
-            console.log("победили нолики!");
+            document.write("победили нолики!");
+            document.write('<br/>')
             stop = 1;
         }    
         else if (board[0][0] == 'o' && board[1][0] == 'o' && board[2][0] == 'o'){ //Проверка столбцов
-            console.log("победили нолики!");
+            document.write("победили нолики!");
+            document.write('<br/>')
             stop = 1;
         }    
         else if (board[0][1] == 'o' && board[1][1] == 'o' && board[2][1] == 'o'){
-            console.log("победили нолики!");
+            document.write("победили нолики!");
+            document.write('<br/>')
             stop = 1;
         }    
         else if (board[0][2] == 'o' && board[1][2] == 'o' && board[2][2] == 'o'){
-            console.log("победили нолики!");
+            document.write("победили нолики!");
+            document.write('<br/>')
             stop = 1;
         }    
         else if (board[0][0] == 'o' && board[1][1] == 'o' && board[2][2] == 'o'){ //Проверка диагоналей
-            console.log("победили нолики!");
+            document.write("победили нолики!");
+            document.write('<br/>')
             stop = 1;
         }    
         else if (board[0][2] == 'o' && board[1][1] == 'o' && board[2][0] == 'o'){
-            console.log("победили нолики!");
+            document.write("победили нолики!");
+            document.write('<br/>')
             stop = 1;
         }    
 
 }
 if (stop == 2){
-    console.log("Ничья");
+    document.write("Ничья");
 }    
 
 
